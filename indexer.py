ce6c84c4490a49eee702083ae019298058af08dd
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-11-12 23:05:03 -0800

    Merging Variant

--- indexer.py
+++ indexer.py
@@ -1,19 +1,44 @@
-import nltk
 import pathlib
 import json
 from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
+import re
 
-source_folder = "sources/ANALYST"
+source_folder = "sources/DEV"
 
 stemmer = PorterStemmer()
 
-def tokenize(string: str) -> set[str]:
-    tokens = set()
-    cleaned = ''.join(char.lower() if char.isalnum() and char.isascii() else ' ' for char in string) 
-    words = cleaned.split()
-    tokens.update(words)
-    return tokens
+class Posting:
+    def __init__(self, docid, tfidf, fields = None):
+        self.docid = docid
+        self.tfidf = tfidf # use freq counts for now
+        self.fields = fields
+    
+def tokenize(content: str) -> list[str]:
+    # cleaned = ''.join(char.lower() if char.isalnum() and char.isascii() else ' ' for char in content) 
+    # words = cleaned.split()
+    return re.findall(r"[a-zA-Z0-9]+", content)
+
+def offload_index(index: dict, docs: int) -> None:
+    index_path = pathlib.Path(f"indexes/index{docs}.json")
+    index_path.parent.mkdir(exist_ok=True, parents=True)
+    with index_path.open("w") as file:
+        json.dump(index, file, indent=4)
+
+def merge_indexes() -> None:
+    indexes_path = pathlib.Path("indexes")
+    paths = list(indexes_path.rglob("*.json"))
+    with open("merged_indexes.json", "w") as file:
+        json.dump(merge_helper(paths), file, indent= 4)
+
+def merge_helper(paths: list[pathlib.Path]) -> dict:
+    if len(paths) == 1:
+        with paths[0].open("r") as file:
+            return json.load(file)
+    else:
+        first_half = merge_helper(paths[:len(paths)//2])
+        second_half = merge_helper(paths[len(paths)//2:])
+        return {key : sorted(first_half.get(key,[]) + second_half.get(key,[]), key=lambda posting: posting["docid"]) for key in first_half.keys() | second_half.keys()}
 
 def index_file(source_folder):
     index = dict()
@@ -21,23 +46,29 @@ def index_file(source_folder):
     paths = p.rglob("*.json")
     docID = 0
     for path in paths:
-        with open(path, "r") as file:
-            data = json.loads(file.readline())
+        with path.open("r") as file:
+            data = json.load(file)
             print(data["url"])
             soup = BeautifulSoup(data["content"], "lxml", from_encoding=data["encoding"])
             tokens = tokenize(soup.get_text())
-            stems = {stemmer.stem(token) for token in tokens}
-            for stem in stems:
+            freqs = {}
+            for token in tokens:
+                freqs[stemmer.stem(token.lower())] = freqs.get(stemmer.stem(token.lower()), 0) + 1
+            # stems = [stemmer.stem(token.lower()) for token in tokens]
+            # unique_stems = set(stems)
+            for stem in freqs.keys():
                 if index.get(stem):
-                    index[stem].append(docID)
+                    index[stem].append(Posting(docID, freqs[stem]).__dict__)
                 else:
-                    index[stem] = [docID,]
+                    index[stem] = [Posting(docID, freqs[stem]).__dict__,]
         docID += 1
-        # if docID > 3:
+        # if docID > 10:
         #     break
-    # print(index)
-    with open("index.json", "w") as f:
-        json.dump(index, f)
+        if docID%1000 == 0:
+            offload_index(index, docID)
+            index=dict()
+    offload_index(index, docID)
+    merge_indexes()
 
 if __name__ == "__main__":
     index_file(source_folder)
