748dfd06c9eef42434ffb77e3cd57db654328fbd
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-11-18 00:24:45 -0800

    Pickle Storing Varient with set intersection

--- analytics.py
+++ analytics.py
@@ -1,14 +1,15 @@
-import json
+import pickle
 import utils.logger
 import os
 import pathlib
+from indexer import Posting
 
-source_folder = "sources/DEV"
+source_folder = "sources/ANALYST"
 
 if __name__ == "__main__":
     logger = utils.logger.get_logger("RESULTS")
-    with open("merged_indexes.json", "r") as file:
-        index = json.load(file)
+    with open("merged_indexes.pkl", "rb") as file:
+        index = pickle.load(file)
         logger.info(f"Total Indexed Documents: {len(list(pathlib.Path(source_folder).rglob('*json')))}")
         logger.info(f"Total Unique Tokens: {len(index)}")
-        logger.info(f"Total Size {os.path.getsize('merged_indexes.json') / 1024} KB")
+        logger.info(f"Total Size {os.path.getsize('merged_indexes.pkl') / 1024} KB")
--- indexer.py
+++ indexer.py
@@ -1,12 +1,13 @@
 import pathlib
 import json
+import pickle
 from bs4 import BeautifulSoup
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 import re
 import utils.logger
 
-source_folder = "sources/DEV"
+source_folder = "sources/ANALYST"
 
 stemmer = PorterStemmer()
 
@@ -17,30 +18,36 @@ class Posting:
         self.docid = docid
         self.tfidf = tfidf # use freq counts for now
         self.fields = fields
+
+    def __eq__(self, other):
+        return self.docid==other.docid
+
+    def __hash__(self):
+        return hash(self.docid)
     
 def tokenize(content: str) -> list[str]:
     return re.findall(r"[a-zA-Z0-9]+", content)
 
 def offload_index(index: dict, docs: int) -> None:
-    index_path = pathlib.Path(f"indexes/index{docs}.json")
+    index_path = pathlib.Path(f"indexes/index{docs}.pkl")
     index_path.parent.mkdir(exist_ok=True, parents=True)
-    with index_path.open("w") as file:
-        json.dump(index, file, indent=4)
+    with index_path.open("wb") as file:
+        pickle.dump(index, file)
 
 def merge_indexes() -> None:
     indexes_path = pathlib.Path("indexes")
-    paths = list(indexes_path.rglob("*.json"))
-    with open("merged_indexes.json", "w") as file:
-        json.dump(merge_helper(paths), file, indent= 4)
+    paths = list(indexes_path.rglob("*.pkl"))
+    with open("merged_indexes.pkl", "wb") as file:
+        pickle.dump(merge_helper(paths), file)
 
 def merge_helper(paths: list[pathlib.Path]) -> dict:
     if len(paths) == 1:
-        with paths[0].open("r") as file:
-            return json.load(file)
+        with paths[0].open("rb") as file:
+            return pickle.load(file)
     else:
         first_half = merge_helper(paths[:len(paths)//2])
         second_half = merge_helper(paths[len(paths)//2:])
-        return {key : sorted(first_half.get(key,[]) + second_half.get(key,[]), key=lambda posting: posting["docid"]) for key in first_half.keys() | second_half.keys()}
+        return {key : sorted(first_half.get(key,[]) + second_half.get(key,[]), key=lambda posting: posting.docid) for key in first_half.keys() | second_half.keys()}
 
 def index_file(source_folder):
     index = dict()
@@ -61,13 +68,13 @@ def index_file(source_folder):
                 if stemmed not in index:
                     index[stemmed] = []
 
-                if index[stemmed] and index[stemmed][-1]["docid"] == docID:
-                    index[stemmed][-1]["freq"] += 1
+                if index[stemmed] and index[stemmed][-1].docid == docID:
+                    index[stemmed][-1].tfidf += 1
                 else:
-                    index[stemmed].append({"docid": docID, "freq": 1})
+                    index[stemmed].append(Posting(docID, 1))
 
         docID += 1
-        if docID%10000 == 0:
+        if docID%200 == 0:
             offload_index(index, docID)
             index=dict()
     offload_index(index, docID)
