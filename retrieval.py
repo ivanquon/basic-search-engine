30a8bd19af2a72cd3051c67cfb576a9dea429086
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-11-27 22:24:57 -0800

    test queries

--- retrieval.py
+++ retrieval.py
@@ -6,12 +6,30 @@ import json
 
 stemmer = PorterStemmer()
 
-#fixed = "masters of software engineering"
-#"ACM"
+
+test_queries = ["ACM", 
+                "master of software engineering", 
+                "cristina lopes", 
+                "machine learning", 
+                "to be or not to be", 
+                "and so on and so forth",
+                "klefstad", 
+                "ray klefstad", 
+                "Donald Bren School of Information and Computer Sciences", 
+                "Quarterly Academic Calendar",
+                "Undergraduate Academic Advising ",
+                "university registrar",
+                "Supercalifragilisticexpialidocious",
+                "Software is everywhere, and every day it becomes a more integrated and indispensable part of how we live. Understanding its inner workings, as well as the complex systems it makes possible, is critical to ongoing technological innovation. By both delving deep into the technical underpinnings of existing systems and continuously experimenting with new software tools and platforms, Professor Cristina Lopes is pioneering the development of innovative, global-scale and immersive environments that help people lead better, fuller and richer lives.",
+                "a",
+                "a the heart"]
+
 #Store shortest posting as a base
 #Intersect each posting list after
 
-def boolean_and(postings: list[list[Posting]]):
+def boolean_and(postings: list[list[Posting]]) -> list:
+    if not postings:
+        return []
     by_length = sorted(postings, key = lambda p: len(p))
     common = by_length[0]
     for postings_list in by_length[1:]:
@@ -46,8 +64,11 @@ if __name__ == "__main__":
             precessed_query = [stemmer.stem(token) for token in tokenize(query)] if unigram else [f"{stemmer.stem(s1)} {stemmer.stem(s2)}" for tokenList in [tokenize(query)] for s1, s2 in zip(tokenList, tokenList[1:])]
 
             for token in precessed_query:
-                index.seek(fastindex[token])
-                postings.append(json.loads(index.readline(), object_hook=posting_decoder)[token])
+                try:
+                    index.seek(fastindex[token])
+                    postings.append(json.loads(index.readline(), object_hook=posting_decoder)[token])
+                except KeyError:
+                    continue
             ranked = sorted([posting for posting in boolean_and(postings)], key=lambda p: p.tfidf, reverse=True)
 
             top_five = [(urls[str(posting.docid)], posting.tfidf) for posting in ranked][:5]
