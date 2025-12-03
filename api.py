6fad39af21f62968309ddc463b7506423910ea39
Ivan Quon <70609060+ivanquon@users.noreply.github.com>
2025-12-02 18:43:01 -0800

    Basic API

---
+++ api.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from fastapi import FastAPI
+from retrieval import retrieve
+import json
+
+app = FastAPI()
+
+with open("fastindex.json", "r") as fast:
+    fastindex = json.load(fast)
+with open("urlmap.json", "r") as urlmap:
+    urls = json.load(urlmap)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/retrieval/{query}")
+def search_query(query: str):
+    results = retrieve(query, fastindex, urls)
+    return {"urls": results[0], "mstime": results[1]}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
--- retrieval.py
+++ retrieval.py
@@ -4,25 +4,43 @@ import datetime
 from indexer import Posting, posting_decoder
 import json
 
+
 stemmer = PorterStemmer()
 
-
-test_queries = ["ACM", 
-                "master of software engineering", 
-                "cristina lopes", 
-                "machine learning", 
-                "to be or not to be", 
-                "and so on and so forth",
-                "klefstad", 
-                "ray klefstad", 
-                "Donald Bren School of Information and Computer Sciences", 
-                "Quarterly Academic Calendar",
-                "Undergraduate Academic Advising ",
-                "university registrar",
-                "Supercalifragilisticexpialidocious",
-                "Software is everywhere, and every day it becomes a more integrated and indispensable part of how we live. Understanding its inner workings, as well as the complex systems it makes possible, is critical to ongoing technological innovation. By both delving deep into the technical underpinnings of existing systems and continuously experimenting with new software tools and platforms, Professor Cristina Lopes is pioneering the development of innovative, global-scale and immersive environments that help people lead better, fuller and richer lives.",
-                "a",
-                "a the heart"]
+test_queries = [
+    #Base Queries
+    "ACM", 
+    "master of software engineering", 
+    "cristina lopes", 
+    "machine learning", 
+    #Difficult Queries with a lot of postings
+    "to be or not to be", 
+    "and so on and so forth",
+    #Names likely found in citations/self page
+    "Michael Shindler", 
+    "ray klefstad",
+    "Cristina Videira Lopes",
+    #General UCI Sites
+    "Donald Bren School of Information and Computer Sciences",
+    #Partial match handling
+    "Quarterly Academic Calendar",
+    "UCI Police Department",
+    "a the heart",
+    #Exact match stress testing
+    "Supercalifragilisticexpialidocious",
+    "Software is everywhere, and every day it becomes a more integrated and indispensable part of how we live. Understanding its inner workings, as well as the complex systems it makes possible, is critical to ongoing technological innovation. By both delving deep into the technical underpinnings of existing systems and continuously experimenting with new software tools and platforms, Professor Cristina Lopes is pioneering the development of innovative, global-scale and immersive environments that help people lead better, fuller and richer lives.",
+    "Introduction to ubiquitous computing research methods, tools and techniques. Prototyping, design and evaluation of physical computing applications, smart environments, embedded systems and future computing scenarios. Includes hands-on in-class laboratory exercises.",
+    #Relevancy of common searches
+    "Undergraduate Academic Advising",
+    "university registrar",
+    "Principles of Data Management",
+    "Fall 2018 Photos",
+    "Video Game Development Club",
+    "Critical Writing on Information Technology",
+    "Project in Ubiquitous Computing",
+    #No Matches
+    "dwajdiowajoisdjowa",
+    ]
 
 #Store shortest posting as a base
 #Intersect each posting list after
@@ -47,7 +65,7 @@ def boolean_and(postings: list[list[Posting]]) -> list:
         common = new_common
     return common
 
-def retrieve(query: str):
+def retrieve(query: str, fastindex, urls):
     start = datetime.datetime.now()
     with open("merged_indexes.json", "r") as index:
         postings = []
@@ -56,6 +74,7 @@ def retrieve(query: str):
         precessed_query = [stemmer.stem(token) for token in tokenize(query)] if unigram else [f"{stemmer.stem(s1)} {stemmer.stem(s2)}" for tokenList in [tokenize(query)] for s1, s2 in zip(tokenList, tokenList[1:])]
 
         for token in precessed_query:
+            #Return as many matches as possible (if full match not available do partial)
             try:
                 index.seek(fastindex[token])
                 postings.append(json.loads(index.readline(), object_hook=posting_decoder)[token])
@@ -65,21 +84,27 @@ def retrieve(query: str):
 
         top_five = [(urls[str(posting.docid)], posting.tfidf) for posting in ranked][:5]
 
-        print("RESULTS")
-        for result in top_five:
-            print(result[0], "with a score of", result[1])
-
     end = datetime.datetime.now()
     total = end - start
-    print("Query finished in", total.microseconds/1000, "ms")
+    return [top_five[:5], total.microseconds/1000]
 
 if __name__ == "__main__":
     with open("fastindex.json", "r") as fast:
         fastindex = json.load(fast)
     with open("urlmap.json", "r") as urlmap:
         urls = json.load(urlmap)
+    for query in test_queries:
+        results = retrieve(query, fastindex, urls)
+        print("RESULTS")
+        for result in results[0]:
+            print(result[0], "with a score of", result[1])
+        print("Query finished in", results[1], "ms")
     while True:
         query = input("Enter your query: ")
         if not query:
             continue
-        retrieve(query)
+        results = retrieve(query, fastindex, urls)
+        print("RESULTS")
+        for result in results[0]:
+            print(result[0], "with a score of", result[1])
+        print("Query finished in", results[1], "ms")
